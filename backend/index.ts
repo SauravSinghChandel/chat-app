59bcf9640488ad214db894d0629276abe15a8f87
Saurav Singh Chandel <sauravca527@gmail.com>
2026-02-12 05:34:35 -0330

    Revert "Added socket server"

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,11 @@
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
-import { createServer } from "http";
-import { initializeSocket } from "./src/utils/socket";
 
 const PORT = process.env.PORT || 3000;
 
-const httpServer = createServer(app);
-
-initializeSocket(httpServer);
-
 connectDB()
     .then(() => {
-        httpServer.listen(PORT, () => {
+        app.listen(PORT, () => {
             console.log("Server is running on PORT: ", PORT);
 
         })
